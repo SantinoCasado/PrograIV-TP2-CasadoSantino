814759599a214c3b282a6e91b5cdb388115acddb
Santino Casado <santinocasado05@gmail.com>
2026-06-01 14:50:53 -0300

    creacion del fondo de logIn y Registro

--- frontend/Aura-frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/Aura-frontend/src/app/features/auth/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CinematicLoginBackground } from '../../../../shared/components/cinta-pelicula-fondo/cinta-pelicula-fondo';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CinematicLoginBackground],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
