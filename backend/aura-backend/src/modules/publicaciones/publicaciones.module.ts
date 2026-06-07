23e293bc31031c593a93397b0c51781f00a0cab9
Santino Casado <santinocasado05@gmail.com>
2026-06-07 15:51:30 -0300

    fix error 500

--- backend/aura-backend/src/modules/publicaciones/publicaciones.module.ts
+++ backend/aura-backend/src/modules/publicaciones/publicaciones.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PublicacionesService } from './publicaciones.service';
 import { PublicacionesController } from './publicaciones.controller';
+import { Publicacion, PublicacionSchema } from './schema/publicacion.schema';
+import { StorageModule } from '../../common/storage/storage.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Publicacion.name, schema: PublicacionSchema }
+    ]),
+    StorageModule,
+  ],
   controllers: [PublicacionesController],
   providers: [PublicacionesService],
 })
