b7c6a72b2571b920b77b8061b3efcbe53e393009
Santino Casado <santinocasado05@gmail.com>
2026-06-14 18:04:05 -0300

    fix navbar

--- frontend/Aura-frontend/src/app/layouts/navbar/navbar.ts
+++ frontend/Aura-frontend/src/app/layouts/navbar/navbar.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  constructor(public authService: AuthService) {}
+}
