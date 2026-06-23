dd8f6f7762ebe301911904b3d0dc0b26fc44b1ae
Santino Casado <santinocasado05@gmail.com>
2026-06-23 19:16:28 -0300

    ADD: JWT modules

--- backend/aura-backend/src/modules/comentarios/comentarios.module.ts
+++ backend/aura-backend/src/modules/comentarios/comentarios.module.ts
@@ -4,13 +4,17 @@ import { ComentariosService } from './comentarios.service';
 import { ComentariosController } from './comentarios.controller';
 import { Comentario, ComentarioSchema } from './schema/comentarios.schema';
 import { Publicacion, PublicacionSchema } from '../publicaciones/schema/publicacion.schema';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Comentario.name, schema: ComentarioSchema },
       { name: Publicacion.name, schema: PublicacionSchema },
     ]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'secreto',
+      signOptions: { expiresIn: '15m' },
+    }),
   ],
   controllers: [ComentariosController],
   providers: [ComentariosService],
--- backend/aura-backend/src/modules/publicaciones/publicaciones.module.ts
+++ backend/aura-backend/src/modules/publicaciones/publicaciones.module.ts
@@ -4,13 +4,17 @@ import { PublicacionesService } from './publicaciones.service';
 import { PublicacionesController } from './publicaciones.controller';
 import { Publicacion, PublicacionSchema } from './schema/publicacion.schema';
 import { StorageModule } from '../../common/storage/storage.module';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Publicacion.name, schema: PublicacionSchema }
     ]),
     StorageModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'secreto',
+      signOptions: { expiresIn: '15m' },
+    }),
   ],
   controllers: [PublicacionesController],
   providers: [PublicacionesService],
