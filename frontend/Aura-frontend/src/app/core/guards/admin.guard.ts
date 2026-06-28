fa86d3aae58d38abecb555e816bec2797e3c2ca2
Santino Casado <santinocasado05@gmail.com>
2026-06-28 19:49:37 -0300

    build del front

--- frontend/Aura-frontend/src/app/core/guards/admin.guard.ts
+++ frontend/Aura-frontend/src/app/core/guards/admin.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -17,19 +16,20 @@ export class AdminGuard implements CanActivate {
     }
 
     try {
-      const decoded: any = jwtDecode(token);
-      
-      // Verifica que el usuario sea administrador
-      if (decoded.role === 'admin' || decoded.rol === 'admin') {
-        return true;
-      } else {
-        // Si no es admin, redirige a publicaciones
-        this.router.navigateByUrl('/publicaciones');
+      // Decodifica el token JWT para obtener la información del usuario
+        const decoded = JSON.parse(atob(token.split('.')[1]));
+        
+        // Verifica que el usuario sea administrador
+        if (decoded.role === 'admin' || decoded.rol === 'admin') {
+            return true;
+        } else {
+            // Si no es admin, redirige a publicaciones
+            this.router.navigateByUrl('/publicaciones');
+            return false;
+        }
+        } catch (error) {
+        this.router.navigateByUrl('/cargando');
         return false;
-      }
-    } catch (error) {
-      this.router.navigateByUrl('/cargando');
-      return false;
+        }
     }
-  }
 }
