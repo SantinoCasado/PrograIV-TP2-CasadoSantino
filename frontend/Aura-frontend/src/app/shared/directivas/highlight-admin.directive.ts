7bc677a9c4dad37878ce117f60b69147918587a4
Santino Casado <santinocasado05@gmail.com>
2026-07-01 09:06:37 -0300

    fix: glow

--- frontend/Aura-frontend/src/app/shared/directivas/highlight-admin.directive.ts
+++ frontend/Aura-frontend/src/app/shared/directivas/highlight-admin.directive.ts
@@ -20,8 +20,9 @@ export class HighlightAdminDirective implements OnInit {
 
   ngOnInit(): void {
     if (this.appHighlightAdmin === 'administrador') {
-      this.el.nativeElement.style.borderColor = '#7fd9ff';
-      this.el.nativeElement.style.boxShadow = '5px 5px 0 #04203b, 0 0 12px rgba(127, 217, 255, 0.2)';
+      this.el.nativeElement.style.borderColor = '#55d4ff';
+      this.el.nativeElement.style.boxShadow = '5px 5px 0 #04203b, 0 0 20px rgba(85, 212, 255, 0.5)';
+      this.el.nativeElement.style.background = 'linear-gradient(140deg, #0d3560, #0c2d4f)';
     }
   }
 }
